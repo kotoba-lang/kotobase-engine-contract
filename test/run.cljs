16012e26b393979ecbcade006096c100fc80fb68
Jun Kawasaki <jun784@gmail.com>
2026-08-11 17:23:03 +0900

    Keep zero-dependency CLJS contract gate isolated

--- test/run.cljs
+++ test/run.cljs
@@ -5,7 +5,6 @@
             [kotobase.engine.conformance-test]
             [kotobase.engine.contract-test]
             [kotobase.engine.identity-test]
-            [kotobase.engine.metadata-test]
             [kotobase.engine.profile-test]
             [kotobase.engine.surface-test]
             [kotobase.engine.qualification-test]))
